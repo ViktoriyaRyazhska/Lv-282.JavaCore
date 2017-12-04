3548236846281b0d7aade43754e7f5425679f729
boka <boka25net@gmail.com>
2017-12-04 15:09:27 +0200

    Inh

--- Inheritance/Bird.java
+++ Inheritance/Bird.java
@@ -1,4 +1,4 @@
-/*Develop abstract class Bird with attributes feathers and layEggs and an abstarct method fly().  Develop classes FlyingBird and NonFlyingBird. Create class Eagle, Swallow, Penguin and Chicken. 
+ /*Develop abstract class Bird with attributes feathers and layEggs and an abstarct method fly().  Develop classes FlyingBird and NonFlyingBird. Create class Eagle, Swallow, Penguin and Chicken. 
 Create array Bird and add different birds to it. 
 Call fly() method for all
 of it. Output the 
--- Inheritance/FlyingBird.java
+++ Inheritance/FlyingBird.java
@@ -1,4 +1,4 @@
-
+  
 public class FlyingBird extends Bird {
 
 	@Override
